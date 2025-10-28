710b38d3348101ca336896e24de98d3048c05fe5
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-10-28 01:00:12 +0100

    Converted requests to use async await

--- randomQouteGenerator/randomQouteGenerator.js
+++ randomQouteGenerator/randomQouteGenerator.js
@@ -17,49 +17,40 @@ function getRandomColorCombo() {
 	const randomIndex = Math.floor(Math.random() * colors.length);
 	return colors[randomIndex];
 }
-function getNewRandomQoute() {
-	fetch("http://api.quotable.io/random")
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error("Network Response was not ok");
-			}
-			return response.json();
-		})
-		.then((data) => {
-			const colorCombo = getRandomColorCombo();
-			const angle = Math.floor(Math.random() * (180 - 45 + 1)) + 45;
-			randomQouteGenerator.style.background =
-				"linear-gradient(" +
-				angle +
-				"deg," +
-				colorCombo[0] +
-				"," +
-				colorCombo[1] +
-				")";
-			// randomQouteGenerator.style.background =
-			// 	"linear-gradient(45deg,#586F7C, #2F3E46)";
+async function getNewRandomQoute() {
+	
+	const response = await fetch("http://api.quotable.io/random");
+	if (!response.ok){
+		alert('There was a problem getting a new qoute!')
+	}
+	const data = await response.json();
 
-			//button hover to adapt to color schema
-			newQuoteButton.addEventListener("mouseover", () => {
-				// newQuoteButton.style.background = colorCombo[1];
-				newQuoteButton.style.color = colorCombo[0];
-				newQuoteButton.style.border = "none";
-			});
-			newQuoteButton.addEventListener("mouseout", () => {
-				newQuoteButton.style.background = "";
-				newQuoteButton.style.color = "";
-				newQuoteButton.style.border = "";
-			});
-			const qouteText = data.content;
-			const qouteAuthor = data.author;
-			document.getElementById("random-qoute-text").innerHTML = qouteText;
-			document.getElementById("random-qoute-author").innerHTML =
-				qouteAuthor;
-		})
-		.catch((error) => {
-			console.error(
-				"There was a problem with the fetch operation: ",
-				error
-			);
-		});
+	const qouteText = data.content;
+	const qouteAuthor = data.author;
+	document.getElementById("random-qoute-text").innerHTML = qouteText;
+	document.getElementById("random-qoute-author").innerHTML = qouteAuthor;
+
+	const colorCombo = getRandomColorCombo();
+	const angle = Math.floor(Math.random() * (180 - 45 + 1)) + 45;
+	randomQouteGenerator.style.background =
+		"linear-gradient(" +
+		angle +
+		"deg," +
+		colorCombo[0] +
+		"," +
+		colorCombo[1] +
+		")";
+
+	//button hover to adapt to color schema
+	newQuoteButton.addEventListener("mouseover", () => {
+
+		newQuoteButton.style.color = colorCombo[0];
+		newQuoteButton.style.border = "none";
+	});
+	newQuoteButton.addEventListener("mouseout", () => {
+	
+		newQuoteButton.style.color = "";
+		newQuoteButton.style.border = "";
+	});
+
 }
