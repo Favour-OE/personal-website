1ebe0bbfc0dcecbadc21ed4ddd265e2c6af74c6e
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-11-12 20:51:43 +0100

    Added stock analysis dashboard basic workflow

---
+++ stockAnalysisDashboard/stockAnalysisDashboard.js
@@ -0,0 +1,23 @@
+const stockAnalysisDashboardInput = "stock-analysis-dashboard-input";
+
+async function analyzeStocks() {
+	document.getElementById("stock-analysis-dashboard-data").innerHTML = "";
+	const stockSymbolToAnalyze = document.getElementById(
+		stockAnalysisDashboardInput
+	).value;
+	if (stockSymbolToAnalyze === 0) {
+		alert("You must put in a ticker symbol before analysis");
+		return;
+	}
+	const url = "http://127.0.0.1:5000/analyze-stock/" + stockSymbolToAnalyze;
+	console.log(url);
+	console.log("Running");
+	const response = await fetch(url);
+	if (!response.ok) {
+		alert("There was a problem getting the analysis for your stock!");
+	}
+	const data = await response.json();
+	console.log(data);
+	document.getElementById("stock-analysis-dashboard-data").innerHTML =
+		JSON.stringify(data);
+}
