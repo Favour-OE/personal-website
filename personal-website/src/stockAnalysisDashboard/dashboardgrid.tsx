485f16b706dec632e6543254a77761cb6ae5cea0
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-11-25 10:30:54 +0100

    refactored dashboard grid code

--- personal-website/src/stockAnalysisDashboard/dashboardgrid.tsx
+++ personal-website/src/stockAnalysisDashboard/dashboardgrid.tsx
@@ -1,11 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from "react";
 import "gridstack/dist/gridstack.min.css";
 import { GridStack } from "gridstack";
 import { DashboardGridContent } from "./stockAnalysisDashboard";
 import NumberStat from "./numberStat";
 import LineChartContent from "./lineChartContent";
-import NewsList from "./newsLinks";
-import NewsSentimentAnalysis from "./newsSentimentAnalysis";
+
 
 function DashboardGrid({ stockData }: { stockData: any }) {
 	useEffect(() => {
@@ -58,39 +58,6 @@ function DashboardGrid({ stockData }: { stockData: any }) {
 						<LineChartContent
 							priceHistory={stockData.priceHistory}
 						></LineChartContent>
-					</DashboardGridContent>
-				</div>
-				<div className="grid-stack-item" gs-w="2" gs-h="2">
-					<DashboardGridContent className="grid-stack-item-content">
-						<div style={{ marginBottom: "10px" }}>
-							Future Earnings
-						</div>
-						{stockData.futureEarningsDates.map(
-							(nextDate: string) => (
-								<div>{nextDate}</div>
-							)
-						)}
-					</DashboardGridContent>
-				</div>
-				{/* Third Row */}
-				<div className="grid-stack-item" gs-w="5" gs-h="2">
-					<DashboardGridContent className="grid-stack-item-content">
-						<NewsList newsLinks={stockData.newsArticles}></NewsList>
-					</DashboardGridContent>
-				</div>
-				<div className="grid-stack-item" gs-w="4" gs-h="2">
-					<DashboardGridContent className="grid-stack-item-content">
-						<NewsSentimentAnalysis
-							newsTextAnalysis={stockData.newsTextAnalysis}
-						></NewsSentimentAnalysis>
-					</DashboardGridContent>
-				</div>
-				<div className="grid-stack-item" gs-w="3" gs-h="2">
-					<DashboardGridContent className="grid-stack-item-content">
-						<img
-							src={`data:image/png;base64,${stockData.newsTextAnalysis.data.wordCloudImage}`}
-							style={{ height: "100%", width: "100%" }}
-						></img>
 					</DashboardGridContent>
 				</div>
 			</div>
